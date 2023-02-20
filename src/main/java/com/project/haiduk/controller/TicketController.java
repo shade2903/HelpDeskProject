f755cba1d8a7c04e7fc7305ee4f04fcf11ae0cac
shade2903 <nighshade2903@gmail.com>
2023-02-20 16:44:39 +0300

    story 2.1

--- src/main/java/com/project/haiduk/controller/TicketController.java
+++ src/main/java/com/project/haiduk/controller/TicketController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,12 +24,30 @@ public class TicketController {
         this.ticketService = ticketService;
     }
 
-    @GetMapping(produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @GetMapping(value = "/all", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<List<TicketDto>> getAllTicket(Principal principal) {
         List<TicketDto> tickets = ticketService.getAllTickets(principal);
         if (tickets.isEmpty()) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
         return new ResponseEntity<>(tickets, HttpStatus.OK);
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<List<TicketDto>> getUserTickets(Principal principal) {
+        List<TicketDto> tickets = ticketService.getAllUserTickets(principal);
+        if (tickets.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(tickets, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<TicketDto> getUserTickets(@PathVariable Long id, Principal principal) {
+        TicketDto ticket = ticketService.getTicketById(id);
+        if (ticket == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(ticket, HttpStatus.OK);
     }
 }
