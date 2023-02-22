8654ee35a43e1eb08cde7ba7ef0d40535ddd458c
shade2903 <nighshade2903@gmail.com>
2023-02-22 10:32:46 +0300

    add converter entities

--- src/main/test/com/project/hiduk/converter/UserConverterTest.java
+++ src/main/test/com/project/hiduk/converter/UserConverterTest.java
@@ -1,14 +1,19 @@
 package com.project.hiduk.converter;
 
+import com.project.haiduk.config.WebAppConfig;
+import com.project.haiduk.converter.AbstractConverter;
 import com.project.haiduk.converter.UserConverter;
 import com.project.haiduk.domain.User;
 import com.project.haiduk.domain.enums.Role;
 import com.project.haiduk.dto.UserDto;
 import ma.glasnost.orika.MapperFacade;
+import ma.glasnost.orika.MapperFactory;
+import ma.glasnost.orika.impl.DefaultMapperFactory;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import static org.junit.Assert.assertEquals;
@@ -20,14 +25,16 @@ public class UserConverterTest {
     @Mock
     MapperFacade mapperFacade;
 
-    
+
     @Before
-    public void init(){
+    public void init() {
         MockitoAnnotations.initMocks(this);
+        mapperFacade = new DefaultMapperFactory.Builder().build().getMapperFacade();
+        mapperFacade.map(User.class, UserDto.class);
     }
 
     @Test
-    public void toDto(){
+    public void toDto() {
         User user = new User();
         user.setId(1L);
         user.setFirstName("Ilon");
@@ -35,14 +42,14 @@ public class UserConverterTest {
         user.setEmail("tesla@mail.com");
         user.setPassword("qwerty");
         user.setRole(Role.MANAGER);
-        System.out.println(user);
+
+        Mockito.when(userConverter.toDto(user)).thenReturn(mapperFacade.map(user, UserDto.class));
 
         UserDto userDto = userConverter.toDto(user);
-        System.out.println(userDto);
         assertEquals(user.getId(), userDto.getId());
         assertEquals(user.getFirstName(), userDto.getFirstName());
         assertEquals(user.getLastName(), userDto.getLastName());
-        assertEquals(user.getEmail(), userDto.getPassword());
+        assertEquals(user.getEmail(), userDto.getEmail());
         assertEquals(user.getPassword(), userDto.getPassword());
         assertEquals(user.getRole(), userDto.getRole());
 
